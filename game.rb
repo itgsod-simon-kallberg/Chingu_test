9a57b53cf2718eaece1420d87cc5ab57eef5e048
Simon Kallberg <Simon.kallberg@elev.it-gymnasiet.se>
2012-11-23 10:24:09 +0100

    Added Player

--- game.rb
+++ game.rb
@@ -7,6 +7,18 @@ class Game < Chingu::Window
 	def initialize
 		super
 		self.input = {esc: :exit}
+		@player = Player.create
+	end
+end
+
+class Player < Chingu::GameObject
+
+	#meta-constructor
+	def setup
+		@x, @y = 350, 400
+		@image = Gosu::Image["staff.png"]
 
 	end
 end
+
+Game.new.show
