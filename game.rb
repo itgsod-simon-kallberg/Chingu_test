82ec34a326b0fc6184e8f052214a2655f9e9708b
Simon Kallberg <Simon.kallberg@elev.it-gymnasiet.se>
2012-11-23 10:40:31 +0100

    Added window

--- game.rb
+++ game.rb
@@ -15,10 +15,43 @@ class Player < Chingu::GameObject
 
 	#meta-constructor
 	def setup
-		@x, @y = 350, 400
+		@x, @y = 750, 400
 		@image = Gosu::Image["staff.png"]
+		# self.factor = 10 #size
+		self.input = {
+			holding_left: :left,
+			holding_right: :right,
+			holding_up: :up,
+			holding_down: :down
+		}
+	end
+
+	def left
+		unless @x - 28 <= 0
+		@x -=1
+	end
+	end
+
+	def right
+		unless @x + 28 >= 800
+		@x +=1
+	end
 
 	end
+
+	def up
+		unless @y - 28 <= 0
+		@y -=1
+		end
+	end
+
+	def down
+		unless @y + 28 >=600
+		@y += 1
+	end
+	end
+
 end
+
 
 Game.new.show
