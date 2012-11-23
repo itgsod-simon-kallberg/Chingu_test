540fb28911d3ed38576f737dcd5b63b8f7e4a0fe
Simon Kallberg <Simon.kallberg@elev.it-gymnasiet.se>
2012-11-23 11:45:41 +0100

    trying to get a labyrinth

--- game.rb
+++ game.rb
@@ -7,48 +7,56 @@ class Game < Chingu::Window
 	def initialize
 		super
 		self.input = {esc: :exit}
+		@background_image = Gosu::Image.new(self, "map2.png", true)
 		@player = Player.create
 	end
+
+	def draw
+		@player.draw
+		@background_image.draw(0,0,0)
+	end
+
 end
 
 class Player < Chingu::GameObject
 
 	#meta-constructor
 	def setup
-		@x, @y = 750, 400
-		@image = Gosu::Image["staff.png"]
-		# self.factor = 10 #size
+		@x, @y = 40, 0
+		@speed = 5
+		@image = Gosu::Image["magic.png"]
+		self.factor = 0.5 #size
 		self.input = {
 			holding_left: :left,
 			holding_right: :right,
 			holding_up: :up,
 			holding_down: :down
 		}
+
 	end
 
 	def left
 		unless @x - 28 <= 0
-		@x -=1
-	end
+	    	@x -= @speed
+		end
 	end
 
 	def right
 		unless @x + 28 >= 800
-		@x +=1
-	end
-
+			@x += @speed
+		end
 	end
 
 	def up
 		unless @y - 28 <= 0
-		@y -=1
+			@y -= @speed
 		end
 	end
 
 	def down
 		unless @y + 28 >=600
-		@y += 1
-	end
+			@y += @speed
+		end
 	end
 
 end
