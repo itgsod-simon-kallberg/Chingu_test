7bfb5135c425c397ca8b448739b3b95b40dfa092
Simon Kallberg <Simon.kallberg@elev.it-gymnasiet.se>
2012-12-07 10:40:36 +0100

    Lybirinth fixed
    yeah

--- game.rb
+++ game.rb
@@ -1,22 +1,42 @@
 require 'chingu'
 
+class Window < Chingu::Window
+	def setup
+		$window.switch_game_state(Game)
+	end
+end
 
-class Game < Chingu::Window
 
+class Game < Chingu::GameState
 	#constructor
+	attr_accessor :gameover
 	def initialize
 		super
 		self.input = {esc: :exit}
 		Maze.new
-		@player = Player.create
+		Player.create
+	end
+		def update
+			super
+			Player.each_bounding_box_collision(Block) do |player, block|
+	 			#pop_game_state
+	 			# game_objects.destroy_all
+	 			Block.destroy_all
+	 			Player.destroy_all	
+	 			$window.switch_game_state(Game)
+	 			#Maze.new
+
+
+	 		Player.each_bounding_box_collision(Finish) do |player, finish|
+	 			
+     	end
 	end
 end
 
 class Maze
-
 	def initialize
 		y = 0
-		File.readlines("labyrint.txt").each do |row|
+		File.readlines("labyrinten.txt").each do |row|
 			x = 0
 			tecken = row.split("")
 			tecken.each do |t|
@@ -25,6 +45,10 @@ class Maze
 					x += 20
 				elsif t == "1"
 					Block.create(x: x, y: y)
+					x += 20
+				
+				elsif t == "2"
+					Finish.create(x: x, y: y)
 					x += 20
 				end
 			end
@@ -35,21 +59,42 @@ class Maze
 end
 
 class Block < Chingu::GameObject
+	has_traits :collision_detection, :bounding_box
+
 	def setup
 		@image = Gosu::Image["block.png"]
 	end
 
+	 def update
+	  	super
+	 		
+     end
+
+end
+
+class Finish < Chingu::GameObject
+	has_traits :collision_detection, :bounding_box
+
+	def setup
+		@image = Gosu::Image["finish.png"]
+	end
+
+	 def update
+	  	super
+	 		
+     end
 
 end
 
 class Player < Chingu::GameObject
+	has_traits :collision_detection, :bounding_box
 
 	#meta-constructor
 	def setup
-		@x, @y = 40, 0
+		@x, @y = 90, 10
 		@speed = 5
-		@image = Gosu::Image["gubbe.png"]
-		# self.factor = 0.2 #size
+		@image = Gosu::Image["halo.png"]
+		self.factor = 1.4 #size
 		self.input = {
 			holding_left: :left,
 			holding_right: :right,
@@ -60,25 +105,25 @@ class Player < Chingu::GameObject
 	end
 
 	def left
-		unless @x - 28 <= 0
+		unless @x - 8 <= 0
 	    	@x -= @speed
 		end
 	end
 
 	def right
-		unless @x + 28 >= 800
+		unless @x + 8 >= 800
 			@x += @speed
 		end
 	end
 
 	def up
-		unless @y - 28 <= 0
+		unless @y - 8 <= 0
 			@y -= @speed
 		end
 	end
 
 	def down
-		unless @y + 28 >=600
+		unless @y + 8 >=600
 			@y += @speed
 		end
 	end
@@ -86,4 +131,4 @@ class Player < Chingu::GameObject
 end
 
 
-Game.new.show
+Window.new.show
